5011c6c56ab0161244e111c8f90f59121c8dd9a8
developerSajib88 <developersajib88@gmail.com>
2025-02-22 16:41:42 +0600

    [IMPLEMENT]: Product repository implementation completed.

--- lib/features/products/data/repositories/product_repository_impl.dart
+++ lib/features/products/data/repositories/product_repository_impl.dart
@@ -1,18 +1,37 @@
+import 'dart:convert';
+
+import 'package:clean_architecture/core/service_locator/service_locator.dart';
+import 'package:clean_architecture/features/products/data/data_source/remote/product_services.dart';
+import 'package:clean_architecture/features/products/data/models/all_products_model.dart';
+import 'package:clean_architecture/features/products/data/models/product_model.dart';
 import 'package:clean_architecture/features/products/domain/repositories/product_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class ProductRepositoryImpl implements ProductRepository{
 
   @override
-  Future<Either> getAllProducts() {
-    // TODO: implement getAllProducts
-    throw UnimplementedError();
+  Future<Either> getAllProducts()async{
+    final returnedData = await sl<ProductService>().fetchAllProducts();
+
+    return returnedData.fold((error) {
+      return Left(error);
+    }, (data){
+      final res = AllProductsModel.fromRawJson(data);
+      return Right(res);
+    });
+
   }
 
   @override
-  Future<Either> searchProducts() {
-    // TODO: implement searchProducts
-    throw UnimplementedError();
+  Future<Either> searchProducts({required String query})async{
+    final returnedData = await sl<ProductService>().searchProduct(query: query);
+    return returnedData.fold(
+        (error){
+          return Left(error);
+        }, (data){
+          final res = ProductModel.fromRawJson(data);
+          return Right(data);
+    });
   }
 
 
--- lib/features/products/domain/repositories/product_repository.dart
+++ lib/features/products/domain/repositories/product_repository.dart
@@ -2,5 +2,5 @@ import 'package:dartz/dartz.dart';
 
 abstract class ProductRepository {
   Future<Either> getAllProducts();
-  Future<Either> searchProducts();
+  Future<Either> searchProducts({required String query});
 }
