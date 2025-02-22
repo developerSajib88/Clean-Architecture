6a13e1f9f9d31d1e9a483b89632ac0a1945f7144
developerSajib88 <developersajib88@gmail.com>
2025-02-22 15:53:17 +0600

    [CREATED]: Api Urls created.

---
+++ lib/core/constants/api_urls.dart
@@ -0,0 +1,14 @@
+class ApiUrls {
+  // Private constructor to prevent instantiation
+  ApiUrls._();
+
+  static const String _baseURL = "https://dummyjson.com";
+
+  // Products Endpoints
+  static String allProducts = "/products";
+  static String searchProducts = "/products/";
+
+  // Public getter to access the base URL
+  static String get baseURL => _baseURL;
+
+}
